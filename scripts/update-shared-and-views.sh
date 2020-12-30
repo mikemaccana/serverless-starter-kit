33bedc0aba7616fae96a6e9cdb2228c10106b31f
Mike MacCana <mike.maccana@gmail.com>
2020-12-30 14:26:31 +0000

    Make links

--- scripts/update-shared-and-views.sh
+++ scripts/update-shared-and-views.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 echo "Watching shared and views directories for changes..."
+
 cd src
-inotifywait -q -m -e modify 'shared' 'views' | while read -r WATCHED_DIR EVENT FILENAME; do
+inotifywait -q -m -e modify,create 'shared' 'views' | while read -r WATCHED_DIR EVENT FILENAME; do
   for LAMBDA in http/*; do 
     ARCHITECT_MODULES_DIR=$LAMBDA/node_modules/@architect
     DESTINATION_DIR=${ARCHITECT_MODULES_DIR}/${WATCHED_DIR}
     [ -d ${DESTINATION_DIR} ] || mkdir -p ${DESTINATION_DIR}; 
-    cp -v ${WATCHED_DIR}${FILENAME} ${DESTINATION_DIR}${FILENAME}
+    # cp -v ${WATCHED_DIR}${FILENAME} ${DESTINATION_DIR}${FILENAME}
+    [ -L ${DESTINATION_DIR}${FILENAME} ] || ln -vs ${WATCHED_DIR}${FILENAME} ${DESTINATION_DIR}${FILENAME} 
   done
 done
