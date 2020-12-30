e1334b6367aa7317f5d64095df44e9e7a4320bd0
Mike MacCana <mike.maccana@gmail.com>
2020-12-30 15:57:31 +0000

    Add websockets to symlink script

--- scripts/update-shared-and-views.sh
+++ scripts/update-shared-and-views.sh
@@ -11,7 +11,7 @@ make-links (){
   # Eg 'authentication.ts'
   FILENAME=$2
   echo "${WATCHED_DIR} ${FILENAME}"
-  for LAMBDA in http/*; do 
+  for LAMBDA in http/* ws/*; do 
     ARCHITECT_MODULES_DIR=$LAMBDA/node_modules/@architect
     DESTINATION_DIR=${ARCHITECT_MODULES_DIR}/${WATCHED_DIR}
     [ -d ${DESTINATION_DIR} ] || mkdir -p ${DESTINATION_DIR}; 
