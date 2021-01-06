155f230fd5b891b3603fe77580e4de49e3239b00
Mike MacCana <mike.maccana@gmail.com>
2021-01-06 13:40:10 +0000

    Ensure sandbox tests consistently use the same port

--- src/http/post-signup/tests/post-signup.test.ts
+++ src/http/post-signup/tests/post-signup.test.ts
@@ -6,22 +6,27 @@ import {
   STATUSES,
 } from "@architect/shared/utils";
 
+const PORT_FOR_SANDBOX = getRandomPortNumberForSandbox();
+
 // See https://arc.codes/docs/en/guides/developer-experience/local-development
 describe(`Can sign up`, () => {
   beforeAll(async () => {
-    await sandbox.start({ port: getRandomPortNumberForSandbox() });
+    await sandbox.start({ port: PORT_FOR_SANDBOX });
   });
 
   test(
     `Can sign up`,
     async () => {
       expect.assertions(2);
-      const response = await postForm("http://127.0.0.1:3333/signup", {
-        email: "unittest@example.com",
-        givenName: "Unit",
-        familyName: "Testerson",
-        password: "BANANAS",
-      });
+      const response = await postForm(
+        `http://127.0.0.1:${PORT_FOR_SANDBOX}/signup`,
+        {
+          email: "unittest@example.com",
+          givenName: "Unit",
+          familyName: "Testerson",
+          password: "BANANAS",
+        }
+      );
 
       expect(response.status).toEqual(STATUSES.Found);
       expect(response.headers.get("location")).toEqual(
